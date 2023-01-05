d16fb6d6b33eedae4326fca9deba6c750c5316a0
roll20plusplus <103390875+roll20plusplus@users.noreply.github.com>
2023-01-05 15:10:35 -0800

    Try that again

--- helper_functions.py
+++ helper_functions.py
@@ -22,10 +22,10 @@ def show_ds_images(ds, class_names):
     plt.figure(figsize=(10, 10))
     for images, labels in ds.take(1):
         for i in range(9):
-          ax = plt.subplot(3, 3, i + 1)
-          plt.imshow(images[i].numpy().astype("uint8"))
-          plt.title(class_names[labels[i]])
-          plt.axis("off")
+            ax = plt.subplot(3, 3, i + 1)
+            plt.imshow(images[i].numpy().astype("uint8"))
+            plt.title(class_names[labels[i]])
+            plt.axis("off")
 
 
 def view_random_image(target_dir, target_class):
