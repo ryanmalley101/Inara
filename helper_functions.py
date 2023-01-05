afacb36aab1c60592896a2037d853d82175e0758
roll20plusplus <103390875+roll20plusplus@users.noreply.github.com>
2023-01-05 15:06:55 -0800

    Upload helpful functions for data analysis in Tensorflow

---
+++ helper_functions.py
@@ -0,0 +1,128 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import random
+import os
+
+
+def show_ds_images(ds, class_names):
+    """
+    Displays a 3x3 array of images from a TensorFlow dataset
+
+    :param ds: tf.data.Dataset of images
+    :param class_names: Array of class names (strings)
+    """
+
+    plt.figure(figsize=(10, 10))
+    for images, labels in ds.take(1):
+    for i in range(9):
+      ax = plt.subplot(3, 3, i + 1)
+      plt.imshow(images[i].numpy().astype("uint8"))
+      plt.title(class_names[labels[i]])
+      plt.axis("off")
+
+
+def view_random_image(target_dir, target_class):
+
+    """
+    Views a random image from a data directory.
+    :param target_dir:
+    :param target_class:
+    :return:
+    """
+    # Setup the target directory (we'll view images from here)
+    target_folder = target_dir + target_class
+
+    # Get a random image path
+    random_image = random.sample(os.listdir(target_folder), 1)
+    print(random_image)
+
+    # Read in the image and plot it using matplotlib
+    img = plt.imread(target_folder + "/" + random_image[0])
+    plt.imshow(img)
+    plt.title(target_class)
+    plt.axis("off")
+
+    print(f"Image shape: {img.shape}")  # Show the shape of the image
+    return img
+
+
+# Create a function to plot time series data
+def plot_time_series(timesteps, values, format='.', start=0, end=None,
+                     label=None):
+    """
+    Plots a timesteps (a series of points in time) against values (a series of values across timesteps).
+
+    Parameters
+    ---------
+    timesteps : array of timesteps
+    values : array of values across time
+    format : style of plot, default "."
+    start : where to start the plot (setting a value will index from start of timesteps & values)
+    end : where to end the plot (setting a value will index from end of timesteps & values)
+    label : label to show on plot of values
+    """
+    # Plot the series
+    plt.plot(timesteps[start:end], values[start:end], format, label=label)
+    plt.xlabel("Time")
+    plt.ylabel("BTC Price")
+    if label:
+        plt.legend(fontsize=14)  # make label bigger
+    plt.grid(True)
+
+def unzip_data(filename):
+  """
+  Unzips filename into the current working directory.
+  Args:
+    filename (str): a filepath to a target zip folder to be unzipped.
+  """
+  zip_ref = zipfile.ZipFile(filename, "r")
+  zip_ref.extractall()
+  zip_ref.close()
+
+
+def walk_through_dir(dir_path):
+    """
+    Walks through dir_path returning its contents.
+    Args:
+      dir_path (str): target directory
+
+    Returns:
+      A print out of:
+        number of subdiretories in dir_path
+        number of images (files) in each subdirectory
+        name of each subdirectory
+    """
+    for dirpath, dirnames, filenames in os.walk(dir_path):
+        print(
+            f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'.")
+
+
+# Function to evaluate: accuracy, precision, recall, f1-score
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+
+
+def calculate_results(y_true, y_pred):
+    """
+    Calculates model accuracy, precision, recall and f1 score of a binary classification model.
+    Args:
+        y_true: true labels in the form of a 1D array
+        y_pred: predicted labels in the form of a 1D array
+    Returns a dictionary of accuracy, precision, recall, f1-score.
+    """
+    # Calculate model accuracy
+    model_accuracy = accuracy_score(y_true, y_pred) * 100
+    # Calculate model precision, recall and f1 score using "weighted average
+    model_precision, model_recall, model_f1, _ = precision_recall_fscore_support(
+        y_true, y_pred, average="weighted")
+    model_results = {"accuracy": model_accuracy,
+                     "precision": model_precision,
+                     "recall": model_recall,
+                     "f1": model_f1}
+    return model_results
